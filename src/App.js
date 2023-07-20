51e2f081767f01f72c64807b52e557ef48fc902a
rishabbehera22 <105111090+rishabbehera22@users.noreply.github.com>
2023-07-20 16:12:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Navbar  from './Navbar'
+import Hero from './Hero'
+import Analyst from './Analyst';
+import Newsletter from './Newsletter';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div >
+   <Navbar></Navbar>
+   <br></br>
+  <Hero></Hero>
+  <br></br>
+  <Analyst></Analyst>
+  <br></br>
+  <Newsletter></Newsletter>
+   </div>
   );
 }
-
-export default App;
+export default  App;
